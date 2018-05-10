4f997ecd1e97acc06ae8e5352434c29dbb6cbbfa
Niko Ehrenfeuchter <nikolaus.ehrenfeuchter@unibas.ch>
2018-05-11 00:37:13 +0200

    Allow BASEDIR to be set from the environment

--- distributions/debian/8_jessie/lxc-create-base.sh
+++ distributions/debian/8_jessie/lxc-create-base.sh
@@ -41,7 +41,7 @@ export SUITE=jessie
 # export MIRROR="http://mirror.switch.ch/ftp/mirror/$DISTRIBUTION/"
 export MIRROR="http://ftp.halifax.rwth-aachen.de/$DISTRIBUTION/"
 
-BASEDIR=/scratch/containers
+BASEDIR="${LXCPATH:-/scratch/containers}"
 TGT_ROOT="$BASEDIR/$VM_HOSTNAME/rootfs"
 TGT_LOCALE="en_US.UTF-8"
 
